6fe7a7c40e1349fd71ed10994e8a6c0780e870ae
Beerspitnight <beerspitnight@gmail.com>
2025-03-14 23:28:10 -0400

    Switched to eventlet for Flask-SocketIO on Heroku

--- app.py
+++ app.py
@@ -10,4 +10,8 @@ def index():
     return "Hello, Illustrator Co-Pilot!"
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+   import eventlet
+eventlet.monkey_patch()
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), allow_unsafe_werkzeug=True)
