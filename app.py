1ac33152f1ca079c5e15f05023e613c9ac8670fe
Beerspitnight <beerspitnight@gmail.com>
2025-03-14 23:35:51 -0400

    Fixed app.py structure for Heroku

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import eventlet
+import os
+
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
@@ -9,9 +13,6 @@ socketio = SocketIO(app)
 def index():
     return "Hello, Illustrator Co-Pilot!"
 
-if __name__ == '__main__':
-   import eventlet
-eventlet.monkey_patch()
-
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), allow_unsafe_werkzeug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
